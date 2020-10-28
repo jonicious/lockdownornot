74da93d1c4e603ec3db5227441eba962223fe730
Jonas Reitmann <jonas@reitmann.me>
2020-10-28 20:44:02 +0100

    Prepend 0 in countdown if necessary

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Countdown from 'react-countdown';
 // 2.11.2020 00:00:00 German time
 const date = new Date(1604271600000)
 
+const formatNumber = number => ("0" + number).slice(-2);
+
 function App() {
 
     return (
         <>
             <h1>Noch nicht, aber bald! Ab 2.11.20.</h1>
             <Countdown date={date} renderer={props => {
-                return <h2>Noch {props.days} Tage {props.hours}:{props.minutes}:{props.seconds}</h2>
+                return <h2>Noch {props.days} Tage {formatNumber(props.hours)}:{formatNumber(props.minutes)}:{formatNumber(props.seconds)}</h2>
             }} />
         </>
     );
